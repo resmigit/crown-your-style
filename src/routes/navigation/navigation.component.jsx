c5c20d1c6ee12f9a82dc9cc076e9cec2882baffc
Resmigit <resmisr.tvpm@gmail.com>
2022-12-07 15:43:37 +0530

    changes for lesson13 with cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import {Fragment,useContext} from 'react';
 import {Outlet,Link} from 'react-router-dom';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import {UserContext} from '../../contexts/user.context';
 import {signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
@@ -27,8 +29,10 @@ const Navigation =() =>{
           {
             currentUser? <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
              :<Link className='nav-link' to='/auth'>Sign In</Link>
-          }            
+          } 
+          <CartIcon></CartIcon>           
           </div>
+          <CartDropdown></CartDropdown>
         </div>
         <Outlet></Outlet>
       </Fragment>
