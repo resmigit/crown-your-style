d71bfacd43a4f8e8dc26d169f1b48b58ca6f9999
Resmigit <resmisr.tvpm@gmail.com>
2022-12-08 14:48:11 +0530

    changes for lesson14 add to cart

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import './product-card.styles.scss';
 import Button from '../button/button.component';
 
 const ProductCard=({product})=>{
     const{id,name,price,imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+    const addProductToCart =() =>addItemToCart(product);
     return(  
         <div className='product-card-container' key={id}>
             <img src={imageUrl} alt={`${name}`}/>
@@ -10,7 +14,7 @@ const ProductCard=({product})=>{
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add To Cart</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>Add To Cart</Button>
         </div>
 )  
 }
