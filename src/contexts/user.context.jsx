181df145e8784104afe6f853b77d3db189f8a9e2
Resmigit <resmisr.tvpm@gmail.com>
2022-12-27 13:49:10 +0530

    changes for lesson26 userContext using useReducer instead of useState

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext,useState,useEffect } from 'react';
+//import { createContext,useState,useEffect } from 'react';
+import { createContext,useEffect,useReducer } from 'react';
 import { onAuthStateChangedListener
         ,createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
@@ -8,8 +9,39 @@ export const UserContext= createContext({
     setCurrentUser:()=> null
 });
 
+export const  USER_ACTION_TYPES = {
+    'SET_CURRENT_USER' : 'SET_CURRENT_USER'
+}
+
+const userReducer =(state,action)=>{
+    console.log('dispatched');
+    console.log(action);
+    const {type, payload} = action;
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return{
+                ...state,
+                currentUser : payload
+            }                  
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+
+}
+
+const INITIAL_STATE ={
+    currentUser : null
+}
+
 export const UserProvider=({children})=>{
-    const [currentUser,setCurrentUser] = useState(null);
+    //const [currentUser,setCurrentUser] = useState(null);
+    //const [state ,dispatch] = useReducer(userReducer,INITIAL_STATE);
+     //const {currentUser} = state;
+    const [{currentUser} ,dispatch] = useReducer(userReducer,INITIAL_STATE);
+    const setCurrentUser = (user) =>{
+        dispatch({type:USER_ACTION_TYPES.SET_CURRENT_USER, payload :user});
+    }
+    console.log(currentUser);
     const value = {currentUser,setCurrentUser};
 
     useEffect(()=>{
@@ -26,3 +58,11 @@ export const UserProvider=({children})=>{
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
+
+/*
+    const userReducer =(state,action) =>{
+        return{
+            currentUser:
+        }        
+    }
+ */
