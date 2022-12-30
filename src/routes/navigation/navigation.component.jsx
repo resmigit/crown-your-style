8a4c953fd1aebcb01985078f168fc7bb80702632
Resmigit <resmisr.tvpm@gmail.com>
2022-12-30 19:29:00 +0530

    changes for lesson27Continued currentUser from redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
-import {Fragment,useContext} from 'react';
+import {Fragment,useContext} from 'react'; 
 //import {Outlet,Link} from 'react-router-dom';
 import {Outlet} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-import {UserContext} from '../../contexts/user.context';
+//import {UserContext} from '../../contexts/user.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { CartContext } from '../../contexts/cart.context';
 import {signOutUser } from '../../utils/firebase/firebase.utils';
 import {NavigationContainer,LogoContainer,NavLinks,NavLink} from './navigation.styles'
@@ -12,7 +14,9 @@ import {NavigationContainer,LogoContainer,NavLinks,NavLink} from './navigation.s
 
 
 const Navigation =() =>{
-  const {currentUser}=useContext(UserContext);
+  // const {currentUser}=useContext(UserContext);
+ // const currentUser = useSelector((state)=>state.user.currentUser);
+  const currentUser = useSelector(selectCurrentUser);
   const{isCartOpen} = useContext(CartContext);
   //const {currentUser,setCurrentUser}=useContext(UserContext);
    //console.log(currentUser);
