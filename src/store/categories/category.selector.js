4155441eb178941c728360b7a2225295a04b6e3e
Resmigit <resmisr.tvpm@gmail.com>
2023-01-10 13:01:05 +0530

    changes for lesson30 using reselect for selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,27 @@
+import {createSelector} from 'reselect';
+
+const selectCategoryReducer = (state)=> state.categories;
+
+export const selectCategories = createSelector(
+                                    [selectCategoryReducer ] , 
+                                    (categoriesSlice ) =>
+                                        categoriesSlice.categories
+                                    );
+
+export const selectCategoriesMap = createSelector(  
+                                        [selectCategories] , 
+                                        (categories) =>                                      
+                                            categories.reduce((acc,category)=>{
+                                            const{title,items} = category;
+                                            acc[title.toLowerCase()] = items;
+                                            return acc;
+                                            },{})                                       
+                                    ) ;                                 
+
 //export const selectCategoriesMap =(state) =>state.categories.categoriesMap;
-export const selectCategoriesMap =(state) =>state.categories.categories
+/* export const selectCategoriesMap =(state) =>state.categories.categories
 .reduce((acc,category)=>{
     const{title,items} = category;
     acc[title.toLowerCase()] = items;
     return acc;
-},{}); 
+},{});  */
