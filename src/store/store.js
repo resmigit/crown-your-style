451d9032be6ec05d385a808ca13436b39ab6f5da
Resmigit <resmisr.tvpm@gmail.com>
2023-01-12 13:09:43 +0530

    changes for lesson33 seperate logger file and adding redux dev tool extension compose

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,11 @@ import { compose, createStore, applyMiddleware} from 'redux';
 import { persistStore,persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 //import { compose, configureStore, applyMiddleware} from 'redux';
-//import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
 //root-reducer
-const loggerMiddleware= (store) =>(next) =>(action)=>{
+/* const loggerMiddleware= (store) =>(next) =>(action)=>{
     if(!action.type){
         return next(action);
     }
@@ -17,7 +17,7 @@ const loggerMiddleware= (store) =>(next) =>(action)=>{
     next(action);
 
     console.log('next state: ',store.getState());
-}
+} */
 
 const persistConfig ={
     key : 'root' ,
@@ -27,8 +27,12 @@ const persistConfig ={
 
 const persistedReducer = persistReducer( persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//const middleWares = [loggerMiddleware];
+const middleWares = [ process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)||
+                        compose;
+//const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 //export const store = createStore(rootReducer,undefined,composedEnhancers);
 export const store = createStore(persistedReducer,undefined,composedEnhancers);
 export const persistor  = persistStore(store);
