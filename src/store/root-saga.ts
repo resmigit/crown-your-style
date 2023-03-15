c0d2f35f5024e3aa30b1656784df303839de2ac1
AjeeshResmi <resmisr.tvpm@gmail.com>
2023-03-15 09:06:29 +0530

    changes for lesson43 saga typing

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,7 +1,11 @@
-import {all,call} from 'redux-saga/effects';
+import {all,call} from 'typed-redux-saga/macro'//'redux-saga/effects';
 import { categoriesSaga } from './categories/category.saga';
 import { userSagas } from './user/user.saga';
 
 export default function* rootSaga(){
-     yield all( [ call(categoriesSaga) ,call(userSagas) ] );
+     yield* all( [ call(categoriesSaga) ,call(userSagas) ] );
 }
+
+/* export default function* rootSaga(){
+     yield all( [ call(categoriesSaga) ,call(userSagas) ] );
+} */
