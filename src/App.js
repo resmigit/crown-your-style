0a31750a0119f51fdd6440df34a034900634cdfe
AjeeshResmi <resmisr.tvpm@gmail.com>
2023-03-21 16:17:05 +0530

    changes for lesson45 auth.jsx corrected in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {Routes,Route} from 'react-router-dom';
 //import { CreateAction } from './utils/reducer/reducer.util';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component.jsx';
+import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component.jsx';
 import Checkout from './routes/checkout/checkout.component';
 import { checkUserSession } from './store/user/user.action';
