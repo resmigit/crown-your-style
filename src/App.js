3df0beaa2cd6743fe6c0f1ab658ce6c30292d052
AjeeshResmi <resmisr.tvpm@gmail.com>
2023-03-21 16:26:46 +0530

    changes for lesson45 corrected shop.jsx in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Routes,Route} from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component.jsx';
+import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 import { checkUserSession } from './store/user/user.action';
 //import { setCurrentUser } from './store/user/user.action';
